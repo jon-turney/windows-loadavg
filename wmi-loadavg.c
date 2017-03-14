dad954f3e6410205c522dc7a8f03becba02ef521
Jon Turney <jon.turney@dronecode.org.uk>
2017-03-14 23:25:40 +0000

    Simpler implementation using PDH

--- wmi-loadavg.c
+++ wmi-loadavg.c
@@ -13,17 +13,23 @@
   actually measure anything, but it should be good enough for programs to
   throttle their activity based on load.
 
-  A per-process load average estimation is maintained.
+  A per-process load average estimate is maintained.
 
-  This estimation is only updated at most every 5 seconds (similar to linux)
+  We attempt to count running and runnable processes, but unlike linux we don't
+  count processes in uninterruptible sleep (blocked on I/O).
 
-  See linux kernel implementation of loadavg, particularly in the tickless
-  kernel case.
+  The number of running processes is estimated as (NumberOfProcessors) * (%
+  Processor Time).  The number of runnable processes is estimated as
+  ProcessorQueueLength.
 
-  We count running and runnable processes, but unlike linux we don't count
-  processes in uninterruptible sleep (blocked on I/O).
+  This estimate is only updated at most every 5 seconds.
 
-  See the MSDN article "Accessing WMI Preinstalled Performance Classes"
+  Note that PDH will only return data for '% Processor Time' afer the second
+  call to PdhCollectQueryData(), as it's computed over an interval, so the first
+  load estimate will always be 0.
+
+  See also the linux kernel implementation of loadavg, particularly in the
+  tickless kernel case.
 */
 
 #include <stdio.h>
@@ -32,44 +38,15 @@
 #include <time.h>
 #include <unistd.h>
 
-/* Emulate MSVCRT-style _(m|)alloca used by comip.h */
-#include <alloca.h>
-#define _alloca alloca
-#define _malloca alloca
-#define _freea(x)
-
-#include <comdef.h>
-#include <wbemidl.h>
+#define _WIN32_WINNT 0x0600
+#include <pdh.h>
 
 static double _loadavg[3] = { 0.0, 0.0, 0.0 };
+static PDH_HQUERY query;
+static PDH_HCOUNTER counter1;
+static PDH_HCOUNTER counter2;
 
-class ProcessorQueueLength
-{
-public:
-  static bool init(void);
-  static void fini(void);
-  static DWORD read(void);
-
-private:
-  static bool try_init(void);
-
-  // data
-  static IWbemLocator *pLoc;
-  static IWbemServices *pNameSpace;
-  static IWbemRefresher *pRefresher;
-  static IWbemConfigureRefresher *pConfig;
-  static IWbemObjectAccess *pAcc;
-  static LONG lProcessorQueueLengthHandle;
-};
-
-IWbemLocator *ProcessorQueueLength::pLoc = NULL;
-IWbemServices *ProcessorQueueLength::pNameSpace = NULL;
-IWbemRefresher *ProcessorQueueLength::pRefresher = NULL;
-IWbemConfigureRefresher *ProcessorQueueLength::pConfig = NULL;
-IWbemObjectAccess *ProcessorQueueLength::pAcc = NULL;
-LONG ProcessorQueueLength::lProcessorQueueLengthHandle = 0;
-
-bool ProcessorQueueLength::init(void)
+static bool load_init(void)
 {
   static bool tried = false;
   static bool initialized = false;
@@ -77,210 +54,84 @@ bool ProcessorQueueLength::init(void)
   if (!tried)
     {
       tried = true;
-      initialized = try_init();
+
+      PDH_STATUS ret = PdhOpenQueryA(NULL, 0, &query);
+      if (ret != ERROR_SUCCESS)
+        return false;
+
+      ret = PdhAddEnglishCounterA(query, "\\Processor(_Total)\\% Processor Time",
+                                  0, &counter1);
+      if (ret != ERROR_SUCCESS)
+        return false;
+
+      ret = PdhAddEnglishCounterA(query, "\\System\\Processor Queue Length",
+                                  0, &counter2);
+      if (ret != ERROR_SUCCESS)
+        return false;
+
+      initialized = true;
     }
 
   return initialized;
 }
 
-bool ProcessorQueueLength::try_init(void)
+/* estimate the current load */
+static double load(void)
 {
-  HRESULT hres;
+  PDH_STATUS ret = PdhCollectQueryData(query);
+  if (ret != ERROR_SUCCESS)
+    return 0.0;
 
-  // Step 1: --------------------------------------------------
-  // Initialize COM. ------------------------------------------
+  /* Estimate the number of running processes as (NumberOfProcessors) * (%
+     Processor Time) */
+  PDH_FMT_COUNTERVALUE fmtvalue1;
+  ret = PdhGetFormattedCounterValue(counter1, PDH_FMT_DOUBLE, NULL, &fmtvalue1);
+  if (ret != ERROR_SUCCESS)
+    return 0.0;
 
-  hres =  CoInitializeEx(0, COINIT_MULTITHREADED);
-  if (FAILED(hres))
-    {
-      printf("Failed to initialize COM library. Error code = 0x%x\n",
-		   hres);
-      return false;
-    }
+  SYSTEM_INFO sysinfo;
+  GetSystemInfo(&sysinfo);
 
-  // Step 2: --------------------------------------------------
-  // Set general COM security levels --------------------------
+  printf("processor time: %f, num processors: %d\n", fmtvalue1.doubleValue,
+         sysinfo.dwNumberOfProcessors);
+  double running = fmtvalue1.doubleValue * sysinfo.dwNumberOfProcessors / 100;
 
-  hres =  CoInitializeSecurity(
-			       NULL,
-			       -1,			      // COM authentication
-			       NULL,			      // Authentication services
-			       NULL,			      // Reserved
-			       RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication
-			       RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation
-			       NULL,			      // Authentication info
-			       EOAC_NONE,		      // Additional capabilities
-			       NULL			      // Reserved
-			       );
-
-  if (FAILED(hres))
-    {
-      printf("Failed to initialize security. Error code = 0x%x\n", hres);
-      CoUninitialize();
-      return false;
-    }
-
-  // Step 3: ---------------------------------------------------
-  // Obtain the initial locator to WMI -------------------------
-
-  hres = CoCreateInstance(
-			  CLSID_WbemLocator,
-			  0,
-			  CLSCTX_INPROC_SERVER,
-			  IID_IWbemLocator, (LPVOID *) &pLoc);
-
-  if (FAILED(hres))
-    {
-      printf("Failed to create IWbemLocator object. Error code = 0x%x\n",
-		   hres);
-      CoUninitialize();
-      return false;
-    }
-
-  // Step 4: -----------------------------------------------------
-  // Connect to WMI through the IWbemLocator::ConnectServer method
-
-  // Connect to the root\cimv2 namespace with
-  // the current user and obtain pointer pSvc
-  // to make IWbemServices calls.
-  hres = pLoc->ConnectServer(
-			     _bstr_t(L"ROOT\\CIMV2"), // Object path of WMI namespace
-			     NULL,			// User name. NULL = current user
-			     NULL,			// User password. NULL = current
-			     0,			// Locale. NULL indicates current
-			     0,			// Security flags.
-			     NULL,			// Authority (for example, Kerberos)
-			     0,			// Context object
-			     &pNameSpace		// pointer to IWbemServices proxy
-			     );
-
-  if (FAILED(hres))
-    {
-      printf("Could not connect. Error code = 0x%x\n", hres);
-      pLoc->Release();
-      CoUninitialize();
-      return false;
-    }
-
-  // Step 5: --------------------------------------------------
-  // Set security levels on the proxy -------------------------
-
-  hres = CoSetProxyBlanket(
-			   pNameSpace,		  // Indicates the proxy to set
-			   RPC_C_AUTHN_WINNT,		  // RPC_C_AUTHN_xxx
-			   RPC_C_AUTHZ_NONE,		  // RPC_C_AUTHZ_xxx
-			   NULL,			  // Server principal name
-			   RPC_C_AUTHN_LEVEL_CALL,	  // RPC_C_AUTHN_LEVEL_xxx
-			   RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
-			   NULL,			  // client identity
-			   EOAC_NONE			  // proxy capabilities
-			   );
-
-  if (FAILED(hres))
-    {
-      printf("Could not set proxy blanket. Error code = 0x%x\n", hres);
-      pNameSpace->Release();
-      pLoc->Release();
-      CoUninitialize();
-      return false;
-    }
-
-  // Step 6: --------------------------------------------------
-
-  // Create a WMI Refresher and get a pointer to the
-  // IWbemConfigureRefresher interface.
-  CoCreateInstance(CLSID_WbemRefresher,
-		   NULL,
-		   CLSCTX_INPROC_SERVER,
-		   IID_IWbemRefresher,
-		   (void**) &pRefresher);
-
-  pRefresher->QueryInterface(IID_IWbemConfigureRefresher,
-			     (void**) &pConfig);
-
-  IWbemClassObject* pObj = NULL;
-
-  // Add the instance to be refreshed.
-  hres = pConfig->AddObjectByPath(
-				  pNameSpace,
-				  L"Win32_PerfRawData_PerfOS_System=@",
-				  0L,
-				  NULL,
-				  &pObj,
-				  NULL);
-
-  if (FAILED(hres))
-    {
-      printf("Cannot add object. Error code: 0x%x\n", hres);
-      pNameSpace->Release();
-      pLoc->Release();
-      CoUninitialize();
-
-      return false;
-    }
-
-  // For quick property retrieval, use IWbemObjectAccess.
-  pObj->QueryInterface(IID_IWbemObjectAccess, (void**) &pAcc);
-
-  // This is no longer required.
-  pObj->Release();
-
-  // Get a property handle for the ProcessorQueueLength property.
-  CIMTYPE variant;
-
-  pAcc->GetPropertyHandle(L"ProcessorQueueLength",
-			  &variant,
-			  &lProcessorQueueLengthHandle);
-
-  return true;
-}
-
-void ProcessorQueueLength::fini(void)
-{
-  // Clean up all the objects.
-  pAcc->Release();
-  pConfig->Release();
-  pRefresher->Release();
-  pNameSpace->Release();
-  pLoc->Release();
-
-  CoUninitialize();
-}
-
-DWORD ProcessorQueueLength::read(void)
-{
-  if (!init())
-    return 0;
-
-  DWORD dwProcessorQueueLength = 0;
-  pRefresher->Refresh(0L);
-  pAcc->ReadDWORD(lProcessorQueueLengthHandle, &dwProcessorQueueLength);
-
-  return dwProcessorQueueLength;
-}
-
-/* XXX: exp is computed more than once */
-#define CALC_LOAD(load, exp, n) \
-  load *= exp; \
-  load += n*(1.0-(exp));
-
-#define EXP(decay_time) \
-  1.0/exp(delta_time/decay_time)
-
-static void calculate_loadavg(int delta_time)
-{
   /* Estimate the number of runnable processes using ProcessorQueueLength */
-  DWORD rql = ProcessorQueueLength::read();
+  PDH_FMT_COUNTERVALUE fmtvalue2;
+  ret = PdhGetFormattedCounterValue(counter2, PDH_FMT_LONG, NULL, &fmtvalue2);
+  if (ret != ERROR_SUCCESS)
+    return 0.0;
 
-  /* Estimate the number of running processes as (NumberOfProcessors) * (% Processor Time) */
+  LONG rql = fmtvalue2.longValue;
 
-  unsigned int active_tasks = rql; /* + XXX: */
-  printf("active_tasks: %u, delta_time %d\n", active_tasks, delta_time);
+  double active_tasks = rql + running;
+  printf("running: %f, rql: %d, active_tasks: %f\n", running, rql, active_tasks);
+
+  return active_tasks;
+}
+
+static void calc_load(int index, int delta_time, int decay_time, double n)
+{
+  double df = 1.0/exp((double)delta_time/decay_time);
+  double load = _loadavg[index];
+  load *= df;
+  load += n*(1.0-df);
+  _loadavg[index] = load;
+}
+
+static void update_loadavg(int delta_time)
+{
+  if (!load_init())
+    return;
+
+  printf("delta_time: %d\n", delta_time);
+
+  double active_tasks = load();
 
   /* Compute the exponentially weighted moving average over ... */
-  CALC_LOAD(_loadavg[0], EXP(60.0),  active_tasks); /* ... 1 min */
-  CALC_LOAD(_loadavg[1], EXP(300.0), active_tasks); /* ... 5 min */
-  CALC_LOAD(_loadavg[2], EXP(900.0), active_tasks); /* ... 15 min */
+  calc_load(0, delta_time, 60,  active_tasks); /* ... 1 min */
+  calc_load(1, delta_time, 300, active_tasks); /* ... 5 min */
+  calc_load(2, delta_time, 900, active_tasks); /* ... 15 min */
 }
 
 /* getloadavg: BSD */
@@ -295,7 +146,7 @@ getloadavg (double loadavg[], int nelem)
   if (delta_time >= 5)
     {
       last_time = curr_time;
-      calculate_loadavg(delta_time);
+      update_loadavg(delta_time);
     }
 
   /* The maximum number of samples is 3 */
@@ -307,14 +158,12 @@ getloadavg (double loadavg[], int nelem)
   return nelem;
 }
 
-
 //
-// g++ -g -O0 wmi-loadavg.c -lole32 -loleaut32 -lwbemuuid -o wmi-loadavg
+// g++ -g -O0 wmi-loadavg.c -o wmi-loadavg -lpdh
 //
 
 int main(int argc, char **argv)
 {
-  printf("Working...\n");
   while (1)
     {
       double la[3];
